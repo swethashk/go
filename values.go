12c3c01d0f3b9f5bbe3297cc00c4d7c94b5cda53
swethashk <36743790+swethashk@users.noreply.github.com>
2025-06-19 10:54:51 +0530

    Create values.go

---
+++ values.go
@@ -0,0 +1,11 @@
+package main
+import fmt
+func main(){
+  fmt.Println("go"+"lang")
+  fmt.Println("1+1=", 1+1)
+  fmt.Println("22/7=", 22.0/7.0)
+  fmt.Println(true && false)
+  fmt.Println(true||false)
+  fmt.Println(!true)
+  
+}
